7a5d880fa4fdc43d8288493e03b939c8fdc5cff6
Mchanech <Mchanech@gmail.com>
2014-07-20 14:55:53 -0400

    First attempt to commit cacheSolve.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Constructs an invertible matrix and caches its inverse
+## for later use
 
-## Write a short comment describing this function
+## Constructs a list with get(), set(), and solve() 
+## functions for a matrix x
 
 makeCacheMatrix <- function(x = matrix()) {
+	m <- NULL
+    	set <- function(y) {
+            x <<- y
+	    m <<- NULL
+	}
+	get <- function() x
+	setinverse <- function(solve) m <<- solve
+	getinverse <- function() m
+	list(set = set, get = get, setinverse = setinverse,
+	     getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
+## Stores the inverse of a matrix in an accessible (cached) variable
+## if there is none already. Otherwise returns the inverted matrix.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-}
+	m <- x$getinverse()
+	if(!is.null(m)) {
+		message("getting inverted matrix")
+		return(m)
+	}
+	data <- x$get()
+	m <- solve(data)
+	x$setinverse(m)
+	m
+	}
